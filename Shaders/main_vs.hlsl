6ab6348e4e3638577bfc759ec801ff16b9f99629
JR Cala <jrcala7@gmail.com>
2026-09-17 05:36:34 +0800

    Physics fix- check first

--- Shaders/main_vs.hlsl
+++ Shaders/main_vs.hlsl
@@ -7,8 +7,8 @@ void main_vs(in VertexInput In, out PSInput Out)
     Out.Pos = mul(viewPos, g_Proj);
     
     Out.WorldPos = worldPos.xyz;
-    Out.Normal = normalize(mul(In.Norm, (float3x3)g_Model));
-    Out.Tangent = normalize(mul(In.Tangent, (float3x3)g_Model));
-    Out.Bitangent = normalize(mul(In.Bitangent, (float3x3)g_Model));
+    Out.Normal = normalize(mul((float3x3) g_Model, In.Norm));
+    Out.Tangent = normalize(mul((float3x3) g_Model, In.Tangent));
+    Out.Bitangent = normalize(mul((float3x3) g_Model, In.Bitangent));
     Out.UV = In.uv;
 }
